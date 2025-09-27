133424aa8df8e31c55f58d71471b8e8ef5afa97f
Immakingthisrblxexploit <alefern0.dilstefan0@gmail.com>
2025-09-27 16:27:39 +0200

    Add files via upload
    strong ware

---
+++ Strongware.bat
@@ -0,0 +1,22 @@
+@echo off
+echo do you want to start strong ware? (Y/N)
+choice /c JN /n /m "press Y for yes or N for fuck no bitch ass nigga:"
+
+if errorlevel 2 goto No
+if errorlevel 1 goto Yes
+
+:yes
+echo starting progam...
+start
+start
+start
+pause
+goto Slut
+
+:No
+well get lost..
+pause
+goto Slut
+
+:Slut
+echo your pc is cooked lil nigga
